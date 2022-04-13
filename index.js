d02c125f222d2f9aec4da3ff4f9356b859cf2534
jctaylor <jctaylor210@gmail.com>
2022-04-13 18:36:01 -0500

    Set up DB

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
+require("dotenv").config();
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
+const User = require("./models/userModel");
+//Import the mongoose module
+var mongoose = require("mongoose");
+
+//Set up default mongoose connection
+var mongoDB = process.env.MONGO_CONNECTION_STRING;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/users", (req, res) => {
+  const user = new User();
+  user.email = req.body.email;
+  user.password_hash = req.body.password_hash;
+  user.save((error) => {
+    if (error) {
+      console.log(error);
+    }
+    res.json({ user });
+  });
 });
 
 app.listen(port, () => {
---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password_hash: { type: String, required: true },
+  show_list: [
+    {
+      id: Number,
+      media_type: String,
+      poster: String,
+      backdrop: String,
+      title: String,
+      date: String,
+      favorited: Boolean,
+      reminder_date: Date,
+      watched: Boolean,
+      review: Number,
+    },
+  ],
+  favorites: [{ show_id: Number }],
+  push_notifications: { type: Boolean },
+  streaming_services: [
+    { provider_id: Number, logo_path: String, provider_name: String },
+  ],
+});
+
+var User = (module.exports = mongoose.model("user", userModel));
+
+module.exports.get = function (callback, limit) {
+  User.find(callback).limit(limit);
+};
