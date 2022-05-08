0a2be3f2bbed80db101b5ada45b2b3aa2e1e8c0c
Bryan Reed <bryan87reed@gmail.com>
2022-05-07 21:03:19 -0700

    Removes push logs

--- push.js
+++ push.js
@@ -43,14 +43,11 @@ const pushNotifications = async () => {
       }
     }
 
-    console.log(messages);
-
     const chunks = expo.chunkPushNotifications(messages);
     const tickets = [];
     for (const chunk of chunks) {
       try {
         const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-        console.log(ticketChunk);
         tickets.push(...ticketChunk);
         // NOTE: If a ticket contains an error code in ticket.details.error, you
         // must handle it appropriately. The error codes are listed in the Expo
